636d6562a52d4888de0983e80d3dbc91734124e0
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-07-17 23:26:00 +0300

    Finished until module 4

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/hello", (req, res) => {
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/")));
+
+app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(PORT, () => {
+  debug(`listening to port ${chalk.green(PORT)}`);
 });
