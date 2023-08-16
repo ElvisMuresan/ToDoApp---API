b8a82c4d7b339f58b30bc4e633583b7a198bbcd1
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-08-16 15:26:57 +0300

    Added the routes folder, gitignore and env. Connected to the MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
 
+const routes = require("./routes/ToDoRoute");
+
+require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => debug(`Connected to MongoDB...`))
+  .catch((err) => debug(err));
+
+app.use(routes);
 
 app.use(morgan("tiny"));
 
