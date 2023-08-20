2cc0b674526a5a0b712d28693e8b4c7d919ee29a
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-08-20 17:58:31 +0300

    Updated and deleted by Id the toDo on server side

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,5 +1,6 @@
 const ToDoModel = require("./models/ToDoModel");
 const debug = require("debug")("app");
+
 module.exports.getToDo = async (req, res) => {
   const toDo = await ToDoModel.find();
   res.send(toDo);
@@ -12,4 +13,18 @@ module.exports.saveToDo = async (req, res) => {
     debug(data);
     res.send(data);
   });
+};
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, title, description, checked } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { title, description, checked })
+    .then(() => res.send("Updated Susscesfully..."))
+    .catch((err) => debug(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Susscesfully..."))
+    .catch((err) => debug(err));
 };
