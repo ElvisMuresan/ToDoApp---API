2636432fe990e731e6fd2c690af32b8495d44f32
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-10-15 14:49:14 +0300

    Installed express-session, moved the passport.js inside the app.js to work

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const chalk = require("chalk");
@@ -6,9 +7,10 @@ const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
-const passport = require("passport");
-
 const routes = require("./routes/ToDoRoute");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const UserModel = require("./controllers/models/UserModel");
 
 require("dotenv").config();
 
@@ -17,7 +19,44 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  session({ secret: "your-secret-key", resave: true, saveUninitialized: true })
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+        if (!user || user.comparePassword(password)) {
+          return done(null, false, { message: "Invalid credentials" });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await UserModel.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
 app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose
   .connect(process.env.MONGODB_URL)
